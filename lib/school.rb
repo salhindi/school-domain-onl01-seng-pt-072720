36f8e8b97c32538f64b7cf4a837b435d46ff3070
Sara A <calisaraalhindi@gmail.com>
2020-08-17 22:03:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+  attr_reader :name, :roster
+  
+  def intialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade].nil?
+      @roster[grade] << name
+  else
+    @roster[grade] = [name]
+    end
+    end
+end
